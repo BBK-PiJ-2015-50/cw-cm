a942a6e35dfc807e9e4f12195b759dc03c515f10
Chris Kimberley <ckimbe02@mail.bbk.ac.uk>
2016-03-10 00:23:42 +0000

    1st test: testID

---
+++ src/impl/ContactImpl.java
@@ -0,0 +1,49 @@
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+/**
+ * Created by Chris Kimberley on 08/03/2016.
+ */
+public class ContactImpl implements Contact {
+    /**
+     * Returns the ID of the contact.
+     *
+     * @return the ID of the contact.
+     */
+    //@Override
+    public int getId() {
+        throw new NotImplementedException();
+    }
+
+    /**
+     * Returns the name of the contact.
+     *
+     * @return the name of the contact.
+     */
+    //@Override
+    public String getName() {
+        throw new NotImplementedException();
+    }
+
+    /**
+     * Returns our notes about the contact, if any.
+     * <p>
+     * If we have not written anything about the contact, the empty
+     * string is returned.
+     *
+     * @return a string with notes about the contact, maybe empty.
+     */
+    //@Override
+    public String getNotes() {
+        throw new NotImplementedException();
+    }
+
+    /**
+     * Add notes about the contact.
+     *
+     * @param note the notes to be added
+     */
+    ///@Override
+    public void addNotes(String note) {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/tests/ContactImplTest.java
@@ -0,0 +1,45 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ * Created by Chris Kimberley on 09/03/2016.
+ */
+public class ContactImplTest {
+    private Contact contact1;
+    private int id1;
+    private String name1, notes1;
+
+    @Before
+    public void setUp() {
+        id1 = 1;
+        name1 = "Joe";
+        notes1 = "1st note";
+
+        contact1 = new ContactImpl(id1, name1, notes1);
+    }
+
+    @Test
+    public void testId() {
+        assertEquals(id1, contact1.getId());
+    }
+
+    @Test
+    public void testGetId() throws Exception {
+
+    }
+
+    @Test
+    public void testGetName() throws Exception {
+
+    }
+
+    @Test
+    public void testGetNotes() throws Exception {
+
+    }
+
+    @Test
+    public void testAddNotes() throws Exception {
+
+    }
+}
