4cc946bbd97fd66af8cddd0daa23ffdc5435aeae
Chris Kimberley <ckimbe02@mail.bbk.ac.uk>
2016-04-09 23:10:29 +0100

    Wrote testMeetingImplConstructorThrowsIllegalArgumentExceptionForNegativeID() - failed as constructor not yet written

---
+++ src/tests/FutureMeetingImplTest.java
@@ -0,0 +1,14 @@
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by chris on 09/04/2016.
+ */
+public class FutureMeetingImplTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+}
--- src/tests/MeetingImplTest.java
+++ src/tests/MeetingImplTest.java
@@ -2,16 +2,32 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by chris on 13/03/2016.
  */
 public class MeetingImplTest {
+    private Meeting meeting1;
+    private Set<Contact> contacts;
+    private Contact contact1;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
+        contacts = new HashSet<>();
+        contact1 = new ContactImpl(1, "John", "VIP" );
+        contacts.add(contact1);
+    }
 
+    @Test (expected = IllegalArgumentException.class)
+    public void testMeetingImplConstructorThrowsIllegalArgumentExceptionForNegativeID() {
+        //meeting1 = new FutureMeetingImpl(-1, Calendar.getInstance(), contacts);
+        meeting1 = new FutureMeetingImpl();
     }
 
     @After
